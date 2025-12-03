c092a6d82b0398bb8d499aa7b2e051ca99e8a133
Andy Rutkowski <andy.rutkowski@gmail.com>
2025-12-03 19:10:56 +0000

    Add config.js with Mapbox token for GitHub Pages deployment

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    mapboxToken: 'pk.eyJ1IjoiYW5keS1ydXRrb3dza2kiLCJhIjoiWDJRMFVUWSJ9.iHl6Cf0M79wmHQlfP6Bk8Q',
+    lotLocation: {
+        lng: -118.29201360294347,
+        lat: 34.021288441298374,
+    },
+    animation: {
+        initialZoom: 10,
+        finalZoom: 19,
+        duration: 8000,
+        transitionDelay: 1000
+    }
+    ,
+    // Vacant land dataset settings
+    showVacantLand: true,
+    vacantLandGeojson: 'assets/data/VacantLand_LA.geojson'
+};
